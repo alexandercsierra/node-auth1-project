7c6685b5c47097d59775075c7883ba24c007776c
Alexander Sierra <alexandercsierra@gmail.com>
2020-02-24 14:31:42 -0500

    started creating server, initialized empty db

---
+++ api/apiRouter.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+// router.use('/auth', authRouter)
+// router.use('/users', usersRouter);
+
+module.exports = router;
---
+++ auth/authRouter.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+//require users model
+
+module.exports = router;
